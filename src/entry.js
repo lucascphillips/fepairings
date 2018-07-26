9ce2ec7f87c2b75543b3b8660c1c53a32a071ac3
lucascphillips <lucasphillipsgt@gmail.com>
2018-07-26 10:27:18 -0400

    added select for fathers

--- src/entry.js
+++ src/entry.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
 import './styles/index.css';
+import Select from 'react-select';
 
 export default class Entry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected_father: ''
+    };
+  }
+
   render() {
+    const { data } = this.props;
     return (
       <div className="row">
         <div className="col-sm-auto">
           <img
             className="char-portrait"
-            src={this.props.data.portrait}
+            src={data.portrait}
             alt="Character Portrait"
           />
-          <figcaption className="portrait-label">
-            {this.props.data.name}
-          </figcaption>
+          <figcaption className="portrait-label">{data.name}</figcaption>
         </div>
         <div className="col-sm">
-          <select />
+          <Select
+            value={this.state.selected_father}
+            options={data.fathers.map(f => ({
+              value: f,
+              label: f
+            }))}
+            onChange={value => this.setState({ selected_father: value })}
+            placeholder="Father"
+            isClearable
+          />
         </div>
         <div className="col-sm" />
       </div>
--- src/lucina.js
+++ src/lucina.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import './styles/index.css';
+import Select from 'react-select';
 
 export default class Lucina extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected_father: ''
+    };
+  }
+
   render() {
-    console.log(this.props.data.lucina);
+    const { data } = this.props;
     return (
       <div className="row">
         <div className="col-sm-auto">
           <img
             className="char-portrait"
-            src={this.props.data.lucina[0].portrait}
+            src={data.lucina[0].portrait}
             alt="Character Portrait"
           />
           <figcaption className="portrait-label">
-            {this.props.data.lucina[0].name}
+            {data.lucina[0].name}
           </figcaption>
         </div>
         <div className="col-sm">
-          <select />
+          <Select
+            value={this.state.selected_father}
+            options={data.lucina[0].fathers.map(f => ({
+              value: f,
+              label: f
+            }))}
+            onChange={value => this.setState({ selected_father: value })}
+            placeholder="Father"
+            isClearable
+          />
         </div>
         <div className="col-sm" />
       </div>
